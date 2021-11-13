36e7eee6338930c6601e61a1a399e8f3f8d56105
khemingkapat <khemingkapat@gmail.com>
2021-11-13 21:45:22 +0700

    await block used for data fetching

--- src/pages/DataChart.svelte
+++ src/pages/DataChart.svelte
@@ -1,22 +1,59 @@
 <script lang="ts">
-	import fetchData from "../scripts/fetchData";
-	import { onMount } from "svelte";
-	let data:Array<Object>
-	// const fetchData = () => {
-	// 	axios
-	// 		.get("https://jsonplaceholder.typicode.com/users")
-	// 		.then((res) => (data = res.data));
-	// };
+	import fetchData, { pmData } from "../scripts/fetchData";
+	import Line from "svelte-chartjs/src/Line.svelte";
 
-	onMount(() => {
-		data = fetchData('http://127.0.0.1:8080/day')
-	});
-
-	console.log(data)
+	let pmDataPromise = fetchData("http://127.0.0.1:8080/day");
 </script>
 
 <div class="flex flex-col">
-<!-- {#each data as user}
-	<p>{user.name}</p>
-{/each} -->
+	{#await pmDataPromise}
+		<p>Waiting For The pmData</p>
+	{:then fetchPMData}
+		<Line
+			data={{
+				labels: fetchPMData.date,
+				datasets: [
+					{
+						label: "Humidity",
+						data: fetchPMData.Humidity,
+						fill: false,
+						borderColor: "lightcoral",
+						pointRadius: 0.5,
+					},
+					{
+						label: "PM1_0",
+						data: fetchPMData.PM1_0,
+						fill: false,
+						borderColor: "#742774",
+
+						pointRadius: 0.5,
+					},
+					{
+						label: "PM2_5",
+						data: fetchPMData.PM2_5,
+						fill: false,
+						borderColor: "#00FF00",
+
+						pointRadius: 0.5,
+					},
+					{
+						label: "PM10_0",
+						data: fetchPMData.PM10_0,
+						fill: false,
+						borderColor: "rgba(75,192,192,1)",
+						pointRadius: 0.5,
+					},
+					{
+						label: "Temperature",
+						data: fetchPMData.Temperature,
+						fill: false,
+						borderColor: "red",
+						pointRadius: 0.5,
+					},
+				],
+				maintainAspectRatio: false,
+			}}
+			options={{ maintainAspectRatio: false }}
+		/>
+	{/await}
 </div>
