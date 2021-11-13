1e57a25aac7079958e0ba9d22a7f42f3fd0a8316
khemingkapat <khemingkapat@gmail.com>
2021-11-13 21:44:44 +0700

    create the async function to fetch pm data

--- src/scripts/fetchData.ts
+++ src/scripts/fetchData.ts
@@ -1,14 +1,39 @@
 import axios from "axios"
 
-const fetchData = (url) => {
-	let data = []
-	axios.get(url)
-		.then((res) => {
-			data = res.data
-			console.log(data)
+export interface pmData {
+	date : Array<String>,
+	Temperature : Array<number>,
+	PM1_0 : Array<number>,
+	PM2_5 : Array<number>,
+	PM10_0 : Array<number>,
+	Humidity : Array<number>
+
+}
+
+const fetchData = async (url) => {
+	let resultData:pmData = {
+		date: [],
+		Humidity: [],
+		PM1_0: [],
+		PM2_5: [],
+		PM10_0: [],
+		Temperature: [],
+	}
+
+
+	await axios.get(url)
+		.then((res) => res.data)
+		.then((data) => {
+			Object.entries(data).forEach(([key, value]) => {
+				resultData.date.push(key)
+				Object.entries(value).forEach(([key, value]) => {
+					resultData[key].push(value)
+				})
+			})
 		})
 
-	return data
+	return resultData
+
 
 }
 
