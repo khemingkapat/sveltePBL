a6f2226fbf2fdb77daa7d81c68b7b694bd1f9794
khemingkapat <khemingkapat@gmail.com>
2021-11-22 22:45:12 +0700

    add the cover flex for side bar

--- src/App.svelte
+++ src/App.svelte
@@ -3,20 +3,21 @@
 	import Header from "./components/Header.svelte";
 	import Hamburger from "./components/Hamburger.svelte";
 	import Sidebar from "./components/Sidebar.svelte";
-	import DataChart from './pages/DataChart.svelte'
+	import DataChart from "./pages/DataChart.svelte";
 	let isOpen: boolean = false;
 </script>
 
-<div class='flex flex-col'>
-<TailwindCss />
-<Hamburger
-	open = {isOpen}
-	on:click={() => {
-		isOpen = !isOpen
-	}}
-/>
-<Sidebar open={isOpen} />
-<Header />
-<DataChart/>
+<div class="flex flex-col">
+	<TailwindCss />
+	<Hamburger
+		open={isOpen}
+		on:click={() => {
+			isOpen = !isOpen;
+		}}
+	/>
+	<div class="w-0 h-screen fixed bg-gray-500 opacity-50 transform duration-500" 
+			class:invisible = {!isOpen}/>
+	<Sidebar open={isOpen} />
+	<!-- <Header /> -->
+	<DataChart />
 </div>
-
